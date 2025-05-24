48575cb605ade9c0118131323dd8a4e4a2b40f33
hautv140802 <hautv1408@gmail.com>
2025-05-24 21:50:53 +0700

    fix: algin center

--- app/components/layout/TopHeader/index.tsx
+++ app/components/layout/TopHeader/index.tsx
@@ -7,7 +7,7 @@ const TopHeader = () => {
     <div className="flex bg-custom-gradient p-[4px_240px] gap-[8px] items-center justify-between">
       <div className="flex gap-[8px] items-center justify-start">
         <Image src={svgs.sale} width={16} height={16} alt="sale" />
-        <span className="font-normal text-[14px] leading-[20px] text-secondary_50 inline-block align-middle">
+        <span className="font-normal text-[14px] leading-[20px] text-secondary_50 inline-block align-middle translate-y-[1px]">
           Nhập mã{" "}
           <span className="font-bold text-[12px] leading-[16px] text-yellow_primary align-middle">
             NEWBIE
@@ -19,7 +19,7 @@ const TopHeader = () => {
       <div className="flex gap-[24px] items-center justify-start">
         <div className="flex gap-[8px] items-center justify-start">
           <Image src={svgs.phoneRounded} width={16} height={16} alt="phone" />
-          <span className="font-normal text-[14px] leading-[20px] text-secondary_50 align-middle">
+          <span className="font-normal text-[14px] leading-[20px] text-secondary_50 align-middle translate-y-[1px]">
             Hotline:{" "}
             <span className="font-bold text-[12px] leading-[16px] text-yellow_primary align-middle">
               0283 760 7607
@@ -29,7 +29,7 @@ const TopHeader = () => {
 
         <div className="flex gap-[8px] items-center justify-start">
           <Image src={svgs.smartphone} width={16} height={16} alt="phone" />
-          <span className="font-normal text-[14px] leading-[20px] text-secondary_50 align-middle">
+          <span className="font-normal text-[14px] leading-[20px] text-secondary_50 align-middle translate-y-[1px]">
             Tải ứng dụng
           </span>
         </div>
